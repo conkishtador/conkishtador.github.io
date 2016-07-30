737b5b899761569acc9c34da7a7dc40cc10228e6
Kishore Shah <kishores339@gmail.com>
2016-07-30 12:40:29 -0700

    Adding final project

---
+++ JS/javascriptfinal2.js
@@ -0,0 +1,38 @@
+"use strict";
+
+$(document).ready(function() {
+  $("#mainpic").owlCarousel({
+      navigation: true,
+      slideSpeed: 300,
+      paginationSpeed: 400,
+      singleItem:true
+  });
+});
+
+// smooth scroll function
+//var smoothscroll = function () {
+// var $("a#about").on ("click", function () {
+//				$("#aboutsection")[0].scrollIntoView ({
+//				behavior:"smooth"
+//});
+//return false;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
